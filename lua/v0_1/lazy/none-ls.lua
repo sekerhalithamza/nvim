3ff649adab3839d2d1a82ba0ef220a07c9303129
sekerhalithamza_wsl <shalithamza@gmail.com>
2024-04-23 17:11:07 +0300

    Add Harpoon

--- lua/v0_1/lazy/none-ls.lua
+++ lua/v0_1/lazy/none-ls.lua
@@ -11,7 +11,7 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettierd,
+				null_ls.builtins.formatting.prettier,
 				require("none-ls.diagnostics.eslint_d"),
 				require("none-ls.code_actions.eslint_d"),
 			},
