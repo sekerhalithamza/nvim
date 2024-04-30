1dfa7b54d1eb091448e92b4d7fd81317e529cf8d
sekerhalithamza_wsl <shalithamza@gmail.com>
2024-04-30 23:17:38 +0300

    feat: show hidden files in oil.nvim

--- lua/v0_1/lazy/oil.lua
+++ lua/v0_1/lazy/oil.lua
@@ -6,8 +6,12 @@ return {
 	},
 
 	config = function()
-		require("oil").setup()
+		require("oil").setup({
+			view_options = {
+				show_hidden = true,
+			},
+		})
 
 		vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>")
-	end
+	end,
 }
